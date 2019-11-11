30dee8d0f4865aad8232ada0b8272b2c7a04d2c7
eltonlazzarin <elton.lazzarin@outlook.com>
2019-11-10 23:14:03 -0300

    Updates

---
+++ precache-manifest.e53dc0a46e49c647974a38dc1f82663a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dadc6075ebf49c770e9605c2ad0d41c4",
+    "url": "/website-portfolio-reactjs/index.html"
+  },
+  {
+    "revision": "ea6ab4ac3c394d125e29",
+    "url": "/website-portfolio-reactjs/static/js/2.3f5297eb.chunk.js"
+  },
+  {
+    "revision": "a2b30306dedd154561bd",
+    "url": "/website-portfolio-reactjs/static/js/main.1bfed40e.chunk.js"
+  },
+  {
+    "revision": "0d7628bb0c4ae7ac0964",
+    "url": "/website-portfolio-reactjs/static/js/runtime-main.ea69b6db.js"
+  },
+  {
+    "revision": "ccd1fcbd7d93af7886ff040ce1397db3",
+    "url": "/website-portfolio-reactjs/static/media/aircnc.ccd1fcbd.png"
+  },
+  {
+    "revision": "f64b56b48c8deaa40b88453d738a5055",
+    "url": "/website-portfolio-reactjs/static/media/coder.f64b56b4.svg"
+  },
+  {
+    "revision": "d655542d528d2c1286c532cd495fad90",
+    "url": "/website-portfolio-reactjs/static/media/designer.d655542d.svg"
+  },
+  {
+    "revision": "6ccaefa9a211b0b8f1b957de88b5ffbb",
+    "url": "/website-portfolio-reactjs/static/media/email.6ccaefa9.svg"
+  },
+  {
+    "revision": "d598c7d7f2ecb42fb4feff4da988d7d0",
+    "url": "/website-portfolio-reactjs/static/media/github.d598c7d7.svg"
+  },
+  {
+    "revision": "5fd896e7b85c2cffdcf3c8fcc63b448f",
+    "url": "/website-portfolio-reactjs/static/media/gobarber.5fd896e7.png"
+  },
+  {
+    "revision": "fcc20f95bd3e511c8a9530e0959e0578",
+    "url": "/website-portfolio-reactjs/static/media/knowledges.fcc20f95.png"
+  },
+  {
+    "revision": "8e0fb3327305bcefccc5612f272d1367",
+    "url": "/website-portfolio-reactjs/static/media/linkedin.8e0fb332.svg"
+  },
+  {
+    "revision": "4a8e3ea0455f682b060811a607427445",
+    "url": "/website-portfolio-reactjs/static/media/logo.4a8e3ea0.svg"
+  },
+  {
+    "revision": "73bb3b8bcf9f8a91d366e66ef41fdb22",
+    "url": "/website-portfolio-reactjs/static/media/website.73bb3b8b.png"
+  }
+]);
